20d52084b39b04f128be58310b22e747e9596d7c
katu <93katu@gmail.com>
2023-10-10 18:00:20 -0300

    filtro respondive cambiado a modal

--- src/components/filters.tsx
+++ src/components/filters.tsx
@@ -3,16 +3,21 @@ import {
   CheckboxGroup,
   Checkbox,
   Button,
-  Dropdown,
-  DropdownTrigger,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
 } from "@nextui-org/react";
 import { useState } from "react";
 
 const Filters = () => {
   const [selected, setSelected] = useState([""]);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   return (
-    <div className="flex gap-3 mb-5">
+    <div className="flex gap-3 md:mb-5">
       <div className="hidden md:flex flex-col">
         <CheckboxGroup
           label="Soñé que..."
@@ -38,36 +43,36 @@ const Filters = () => {
       </div>
 
       <div className="md:hidden">
-        <Dropdown>
-          <DropdownTrigger>
-            <Button variant="flat" className="capitalize">
-              Filtros
-            </Button>
-          </DropdownTrigger>
-          <div className="border border-gray-50 rounded-3xl p-5 bg-darkBlue">
-            <CheckboxGroup
-              label="Soñé que..."
-              color="warning"
-              value={selected}
-              onValueChange={setSelected}
-            >
-              {filtros.map((filter) => (
-                <Checkbox key={filter.value} value={filter.value}>
-                  {filter.key}
-                </Checkbox>
-              ))}
-            </CheckboxGroup>
-            <Button
-              className="mt-3"
-              color="primary"
-              variant="bordered"
-              onClick={() => setSelected([""])}
-              isDisabled={selected.length < 2}
-            >
-              Resetear
-            </Button>
-          </div>
-        </Dropdown>
+        <Button onPress={onOpen} variant="shadow" color="primary">Filtros</Button>
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+          <ModalContent>
+            {(onClose) => (
+                <ModalBody>
+                  <CheckboxGroup
+                    label="Soñé que..."
+                    color="warning"
+                    value={selected}
+                    onValueChange={setSelected}
+                  >
+                    {filtros.map((filter) => (
+                      <Checkbox key={filter.value} value={filter.value}>
+                        {filter.key}
+                      </Checkbox>
+                    ))}
+                  </CheckboxGroup>
+                  <Button
+                    className="mt-3"
+                    color="primary"
+                    variant="bordered"
+                    onClick={() => setSelected([""])}
+                    isDisabled={selected.length < 2}
+                  >
+                    Resetear
+                  </Button>
+                </ModalBody>
+            )}
+          </ModalContent>
+        </Modal>
       </div>
     </div>
   );
